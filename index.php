416eb06a5f5ef15e48b39c414881015280aeb730
EnzoTecheira <enzo.techeira@gmail.com>
2018-03-30 21:09:36 -0300

    Creacion del index con banner y nav

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fox Designs</title>
+	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Styles -->
+	<link rel="stylesheet" type="text/css" href="./css/styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+
+		<!-- HEADER START -->
+		<header>
+			<div class="logo"><img src="./images/logo-foxdesigns.png"></div>
+			<nav>
+				<ul>
+					<li class="hover-center"><a href="#">HOME</a></li>
+					<li class="hover-center"><a href="#about-us">ABOUT US</a></li>
+					<li class="hover-center"><a href="#services">SERVICES</a></li>
+					<li class="hover-center"><a href="#contact">CONTACT</a></li>
+				</ul>
+			</nav>
+		</header>
+		<!-- HEADER END -->
+
+		<!-- BANNER START -->
+		<div class="banner">
+			<h1>Web Designers</h1>
+			<h4>#contentmakers</h4>
+			<a href="#contact"><button>Contact Us</button></a>
+		</div>
+		<!-- BANNER END -->
+	</div>
+</body>
+</html>
